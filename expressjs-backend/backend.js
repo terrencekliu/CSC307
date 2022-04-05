bdfd384cfce586734bf072815072de206e87dc2e
Terrence Liu <tliu29@calpoly.edu>
2022-04-04 19:59:08 -0700

    added list users

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -36,8 +36,19 @@ const users = {
 app.use(express.json());
 
 app.get('/users', (req, res) => {
-  res.send(users);
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = {users_list: result};
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
+
+const findUserByName = (name) => {
+  return users['users_list'].filter( (user) => user['name'] === name);  
+}
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
