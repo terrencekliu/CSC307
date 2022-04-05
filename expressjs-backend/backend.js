15313a62ec374a75d9177e5419dae7f8ed23182e
Terrence Liu <tliu29@calpoly.edu>
2022-04-04 20:02:00 -0700

    get users by id

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -35,16 +35,21 @@ const users = {
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = {users_list: result};
-    res.send(result);
-  } else {
-    res.send(users);
+app.get('/users/:id', (req, res) => {
+  const id = req.params['id']; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined || result.length == 0)
+      res.status(404).send('Resource not found.');
+  else {
+      result = {users_list: result};
+      res.send(result);
   }
 });
+
+function findUserById(id) {
+  return users['users_list'].find( (user) => user['id'] === id); // or line below
+  //return users['users_list'].filter( (user) => user['id'] === id);
+}
 
 const findUserByName = (name) => {
   return users['users_list'].filter( (user) => user['name'] === name);  
