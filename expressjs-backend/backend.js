5a3e68880060ab6f6c219df3d5ded6873d1e8f10
Terrence Liu <tliu29@calpoly.edu>
2022-04-04 20:20:23 -0700

    POST method implemented

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -58,3 +58,13 @@ const findUserByName = (name) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });   
+
+app.post('/users', (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.status(200).end();
+});
+
+function addUser(user){
+  users['users_list'].push(user);
+}
