93340f8fc5106da15e1ea2abf373919562407e4a
Terrence Liu <tliu29@calpoly.edu>
2022-04-11 09:33:00 -0700

    completed all tasks

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -5,15 +5,16 @@ import axios from 'axios';
 
 
 function MyApp() {
+  const [characters, setCharacters] = useState([]);
+
   async function fetchAll(){
     try {
-       const response = await axios.get('http://localhost:5001/users');
-       return response.data.users_list;     
+      const response = await axios.get('http://localhost:5001/users');
+      return response.data.users_list;     
     }
     catch (error){
-       //We're not handling errors. Just logging into the console.
-       console.log(error); 
-       return false;         
+      console.log(error); 
+      return false;         
     }
   }
 
@@ -31,15 +32,13 @@ function MyApp() {
   async function makePostCall(person){
     try {
       const response = await axios.post('http://localhost:5001/users', person);
-      return response.data;
+      return response;
     }
     catch (error) {
        console.log(error);
        return false;
     }
   }
-
-  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     makeDeleteCall(characters[index]).then(result => {
@@ -52,19 +51,20 @@ function MyApp() {
     })
   }
 
- function updateList(person) { 
-  makePostCall(person).then( result => {
-  if (result && result.status === 201)
-     setCharacters([...characters, person] );
-  });
- }
+  function updateList(person) {
+    const response = makePostCall(person);
+    response.then( result => {
+      if (result && result.status === 201)
+        setCharacters([...characters, result.data] );
+      });
+  }
 
-useEffect(() => {
-  fetchAll().then( result => {
-     if (result)
-        setCharacters(result);
-   });
-}, [] );
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+          setCharacters(result);
+    });
+  }, [] );
 
   return (
     <div className="container">
