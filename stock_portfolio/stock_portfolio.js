658f3e4ad2997f6299c2bafb840bd81c535b4619
Terrence Liu <tliu29@calpoly.edu>
2022-04-15 14:36:32 -0700

    Final stock portfolio with TDD

--- stock_portfolio/stock_portfolio.js
+++ stock_portfolio/stock_portfolio.js
@@ -4,32 +4,32 @@ function Stock(numShare, ticketSym) {
   return {numShare: numShare, ticketSym: ticketSym};
 }
 
-function findStock(targetStock) {
-  return portfolio.findIndex( (element) => element.ticketSym === targetStock.ticketSym);
+function findStock(ticketSym) {
+  return portfolio.findIndex( (element) => element.ticketSym === ticketSym);
 }
 
-function purchaseStock(newStock) {
-  const i = findStock(newStock);
-  if (i > -1) portfolio[i].numShare += newStock.numShare;
-  else portfolio.push(newStock);
+function purchaseStock(numShare, ticketSym) {
+  const i = findStock(ticketSym);
+  if (i === -1) portfolio.push(Stock(numShare, ticketSym));
+  else portfolio[i].numShare += numShare;
 }
 
-function saleStock(targetStock) {
-  const i = findStock(targetStock);
-  if (i < 0) throw new Error("ticket symbol does not exist");
+function sellStock(numShare, ticketSym) {
+  const i = findStock(ticketSym);
+  if (i === -1) throw new Error("ticket symbol does not exist");
 
-  if ((portfolio[i].numShare - targetStock.numShare) > 0) {
-    portfolio[i].numShare -= targetStock.numShare;
-  } else if ((portfolio[i].numShare - targetStock.numShare) === 0) {
+  if ((portfolio[i].numShare - numShare) > 0) {
+    portfolio[i].numShare -= numShare;
+  } else if ((portfolio[i].numShare - numShare) === 0) {
     portfolio.splice(i, 1);
   } else {
     throw new ShareSaleException("selling more share(s) than own");
   }
 }
 
-function totalShareOfSym(sym) {
-  const i = findStock(Stock(null, sym));
-  if (i < 0) throw new Error("ticket symbol does not exist");
+function totalShareOfSym(ticketSym) {
+  const i = findStock(ticketSym);
+  if (i === -1) throw new Error("ticket symbol does not exist");
   else return portfolio[i].numShare;
 }
 
@@ -53,6 +53,6 @@ exports.getPortfolio = getPortfolio;
 exports.Stock = Stock;
 exports.numUniqueTicketSym = numUniqueTicketSym;
 exports.purchaseStock = purchaseStock;
-exports.saleStock = saleStock;
+exports.sellStock = sellStock;
 exports.totalShareOfSym = totalShareOfSym;
 exports.cleanPortfolio = cleanPortfolio;
--- stock_portfolio/stock_portfolio.test.js
+++ stock_portfolio/stock_portfolio.test.js
@@ -14,23 +14,22 @@ test('Testing numUniqueTicketSym, no unique ticketSym -- success', () => {
 
 test('Testing cleanPortfolio -- success', () => {
   stockPortfolio.cleanPortfolio();
+
   const target = [];
   const result = stockPortfolio.getPortfolio();
   expect(target).toEqual(result);
 });
 
 test('Testing purchaseStock, insert one ticketSym -- success', () => {
-  const newStock = stockPortfolio.Stock(5, "VTI");
-  stockPortfolio.purchaseStock(newStock);
+  stockPortfolio.purchaseStock(5, "VTI");
 
-  const target = [newStock];
+  const target = [stockPortfolio.Stock(5, "VTI")];
   const result = stockPortfolio.getPortfolio();
   expect(target).toEqual(result);
 });
 
 test('Testing purchaseStock, insert another w/same ticketSym -- success', () => {
-  const newStock = stockPortfolio.Stock(5, "VTI");
-  stockPortfolio.purchaseStock(newStock);
+  stockPortfolio.purchaseStock(5, "VTI");
 
   const target = [stockPortfolio.Stock(10, "VTI")];
   const result = stockPortfolio.getPortfolio();
@@ -38,40 +37,35 @@ test('Testing purchaseStock, insert another w/same ticketSym -- success', () => 
 });
 
 test('Testing purchaseStock, insert another w/different ticketSym -- success', () => {
-  const newStock = stockPortfolio.Stock(5, "VOO");
-  stockPortfolio.purchaseStock(newStock);
+  stockPortfolio.purchaseStock(5, "VOO");
 
   const target = [stockPortfolio.Stock(10, "VTI"), stockPortfolio.Stock(5, "VOO")];
   const result = stockPortfolio.getPortfolio();
   expect(target).toEqual(result);
 });
 
-test('Testing saleStock, sell w/same ticketSym -- success', () => {
-  const targetStock = stockPortfolio.Stock(5, "VTI");
-  stockPortfolio.saleStock(targetStock);
+test('Testing sellStock, sell w/same ticketSym -- success', () => {
+  stockPortfolio.sellStock(5, "VTI");
 
   const target = [stockPortfolio.Stock(5, "VTI"), stockPortfolio.Stock(5, "VOO")];
   const result = stockPortfolio.getPortfolio();
   expect(target).toEqual(result);
 });
 
-test('Testing saleStock, sell all one ticketSym -- success', () => {
-  const targetStock = stockPortfolio.Stock(5, "VTI");
-  stockPortfolio.saleStock(targetStock);
+test('Testing sellStock, sell all one ticketSym -- success', () => {
+  stockPortfolio.sellStock(5, "VTI");
 
   const target = [stockPortfolio.Stock(5, "VOO")];
   const result = stockPortfolio.getPortfolio();
   expect(target).toEqual(result);
 });
 
-test('Testing saleStock -- selling more share(s) than own', () => {
-  const targetStock = stockPortfolio.Stock(10, "VOO");
-  expect(() => stockPortfolio.saleStock(targetStock)).toThrow(/selling/);
+test('Testing sellStock -- selling more share(s) than own', () => {
+  expect(() => stockPortfolio.sellStock(10, "VOO")).toThrow(/selling more share/);
 });
 
-test('Testing saleStock -- ticket symbol does not exist', () => {
-  const targetStock = stockPortfolio.Stock(10, "GME");
-  expect(() => stockPortfolio.saleStock(targetStock)).toThrow(/exist/);
+test('Testing sellStock -- ticket symbol does not exist', () => {
+  expect(() => stockPortfolio.sellStock(10, "GME")).toThrow(/does not exist/);
 });
 
 test('Testing totalShareOfSym -- success', () => {
@@ -81,16 +75,14 @@ test('Testing totalShareOfSym -- success', () => {
 });
 
 test('Testing totalShareOfSym -- ticket symbol does not exist', () => {
-  expect(() => stockPortfolio.totalShareOfSym("GME")).toThrow(/exist/);
+  expect(() => stockPortfolio.totalShareOfSym("GME")).toThrow(/does not exist/);
 });
 
 test('Testing cleanPortfolio -- success', () => {
-  const targetStock = stockPortfolio.Stock(0, "GME");
-  stockPortfolio.purchaseStock(targetStock);
-  // now, portfolio = [(5, "VOO"), (0, "GME")]
+  stockPortfolio.purchaseStock(0, "GME");
+  stockPortfolio.cleanPortfolio();
 
   const target = [stockPortfolio.Stock(5, "VOO")];
-  stockPortfolio.cleanPortfolio();
   const result = stockPortfolio.getPortfolio();
   expect(target).toEqual(result);
 });
